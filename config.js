01de572f2eebf99d1b7de1c3e4cfedf5d1308d11
Doansama02 <85267860+Doansama02@users.noreply.github.com>
2021-06-04 10:25:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,22 @@
 const CONFIG = {
     introTitle: 'Em à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Chờ đợi bao lâu anh không nói ra 
+    nghe trong lòng ôi sao xót xa 
+    vì sao khoảng cách đôi ta xa quá 
+    biết đâu tình yêu ta mới nhạt nhòa 
+    vẫn nơi đây chỉ còn mỗi anh
+    và ngày buồn mang theo cơn gió lạnh
+    bao thử thách vẫn ở phía trước 
+    liệu rằng ta có vững bước vượt qua `,
     btnIntro: 'chuối lùn',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    btnAccept: 'Okiiiii lun <3',
+    title: ' Chỉ là vài câu yêu thương thế thôi😙',
+    desc: 'Làm sao cất tiếng nói ',
+    btnYes: 'Anh yêu em nhiều lắm ',
+    btnNo: 'Em yêu anh nhiều lắm  :3',
+    question:'Gửi cho anh',
+    btnReply: 'Yêu mỗi em trong đời',
+    reply: 'Chỉ biết lặng im ôi sao bối rối',
+    mess: 'Dẫu cho con tim rung động rồi. 😘😘',
+    btnAccept: 'I LOVE YOU',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
